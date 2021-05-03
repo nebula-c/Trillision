60d3d9b3a735d4c9c2f0e37eafb08ab2de445712
Sungun Choi <schoi@hipexs-Mac-Pro.local>
2021-05-03 17:54:25 +0900

    ver origin

--- OneParticle/build/CMakeFiles/PROJECT.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/PROJECT"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/PROJECT.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
